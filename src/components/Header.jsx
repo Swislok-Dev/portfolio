254697e2b8b2eacce0fd3d7f01331ff236d3bab4
Swislok-Dev <cmarker4@gmail.com>
2022-09-02 07:59:35 -0500

    create navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,36 @@
 import React from "react"
 
 function Header() {
+  
+  
   return (
-      <header className="border">This is the header</header>
-    )
+      <header className="border">
+        <nav className="navbar">
+          <a href="#cory" className="nav-branding">Cory</a>
+          
+          <ul className="nav-menu">
+            <li className="nav-item">
+              <a href="#about" className="nav-link">About</a>
+            </li>
+            <li className="nav-item">
+              <a href="#projects" className="nav-link">Projects</a>
+            </li>
+            <li className="nav-item">
+              <a href="#skills" className="nav-link">Skills</a>
+            </li>
+            <li className="nav-item">
+              <a href="#contact" className="nav-link">Contact</a>
+            </li>
+          </ul>
+          
+          <div className="hamburger">
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div>
+        </nav>
+      </header>
+  )
 }
 
 export default Header
