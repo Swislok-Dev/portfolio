b5e386232f298828274318fbfa63aeaf6c15ffc5
Swislok-Dev <cmarker4@gmail.com>
2022-09-02 08:01:06 -0500

    import components to the App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from "./components/Header"
+import PageHero from "./components/PageHero";
+import Projects from "./components/Projects";
 
 function App() {
   return (
       <main>
         <Header />
+        <PageHero />
+        <Projects />
       </main>
   );
 }
