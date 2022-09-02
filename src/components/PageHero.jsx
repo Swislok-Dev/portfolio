b61b0120b5eee8eb81f12feaba1e4c2e3c8740ad
Swislok-Dev <cmarker4@gmail.com>
2022-09-02 07:58:03 -0500

    add initial content

---
+++ src/components/PageHero.jsx
@@ -0,0 +1,12 @@
+function PageHero() {
+  return (
+      <section className="border">
+        <div className="content page-hero">
+          <h1>Cory Marker</h1>
+          <h2>Developer</h2>
+        </div>
+      </section>
+  )
+}
+
+export default PageHero
