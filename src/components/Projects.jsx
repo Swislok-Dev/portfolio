3a6524ce8fa7fc21b0c73ef64ae49b1aa2afbe0e
Swislok-Dev <cmarker4@gmail.com>
2022-09-02 07:58:50 -0500

    add project content

---
+++ src/components/Projects.jsx
@@ -0,0 +1,25 @@
+
+export default function Projects() {
+  return (
+      <section className="border">
+        <div className="content projects">
+          <h2>Projects</h2>
+          <p>
+            My projects will go here should be a scroll to get to them
+          </p>
+          <article>
+            <h3>Flight Tracker</h3>
+            <h4>
+              Airline flight tracker to view current status of an upcoming flight
+            </h4>
+            <ul className="project-details article-list">
+              <li>Built using Express.js server framework for API calls</li>
+              <li>Generated UI using React</li>
+              <li>Utilized Redux for global state management</li>
+            </ul>
+            <img src={require("../assets/photos/flight-tracker-image.png")} alt="Flight Tracker Landing page" />
+          </article>
+        </div>
+      </section>
+  )
+}
