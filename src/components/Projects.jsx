939d19829ec83471a1d10104aafc05124d9a21b0
Swislok-Dev <cmarker4@gmail.com>
2022-09-06 11:38:10 -0500

    add project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,25 +1,44 @@
-
 export default function Projects() {
   return (
-      <section className="projects">
-        <div className="content">
-          <h2>Projects</h2>
-          <p>
-            My projects will go here should be a scroll to get to them
-          </p>
-          <article>
-            <h3>Flight Tracker</h3>
-            <h4>
-              Airline flight tracker to view current status of an upcoming flight
-            </h4>
-            <ul className="project-details article-list">
-              <li>Built using Express.js server framework for API calls</li>
-              <li>Generated UI using React</li>
-              <li>Utilized Redux for global state management</li>
-            </ul>
-            <img src={require("../assets/photos/flight-tracker-image.png")} alt="Flight Tracker Landing page" />
-          </article>
-        </div>
-      </section>
+    <section className="projects">
+      <h2 className="projects-heading">Projects</h2>
+      <div className="content">
+        <article class="project article">
+          <h3>Flight Tracker</h3>
+          <h4 className="project-description">
+            Airline flight tracker to view current status of an upcoming flight
+          </h4>
+          <span>Built using:</span>
+          <ul className="project-details article-list">
+            <li>Express.js server framework for API calls</li>
+            <li>Generated UI using React</li>
+            <li>Utilized Redux for global state management</li>
+          </ul>
+          <img
+            src={require("../assets/photos/flight-tracker-image.png")}
+            alt="Flight Tracker Landing page"
+            class="project-image"
+          />
+        </article>
+        <article className="project article">
+          <h3>Guitar Picks</h3>
+          <h4 className="project-description">
+            Allows users to submit pictures of guitars
+          </h4>
+          <span>Built using:</span>
+          <ul className="project-details article-list">
+            <li>JavaScript frontend</li>
+            <li>Ruby login validations</li>
+            <li>Rails API</li>
+            <li>PostgreSQL</li>
+          </ul>
+          <img
+            src={require("../assets/photos/guitar-picks-image.png")}
+            alt="guitar-picks logo"
+            className="project-image"
+          />
+        </article>
+      </div>
+    </section>
   )
 }
