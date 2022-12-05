ad938b4140b554468f2378f8a5c7bbf8237747fa
Swislok-Dev <cmarker4@gmail.com>
2022-12-05 11:32:30 -0600

    redirect to new project deployment

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,34 +1,37 @@
-import Project from "./Project"
+import Project from './Project';
 
 export default function Projects() {
   const flightTrackerItems = [
-    "Express.js server framework for API calls",
-    "Generated UI using React",
-    "Utilized Redux for global state management",
-  ]
+    'Next.js',
+    'Tailwindcss',
+    'Typescript',
+    'Axios API requests',
+  ];
   const flightTrackerDescription =
-    "Airline flight tracker to view current status of an upcoming flight"
-  const flightTrackerImage = require("../assets/photos/flight-tracker-image.jpeg")
-  const flightTrackerGithubURL = "https://github.com/Swislok-Dev/Airline-Tracker"
-  const flightTrackerLiveURL = "http://airline-tracker.herokuapp.com/"
+    'Airline flight tracker to view current status of an upcoming flight';
+  const flightTrackerImage = require('../assets/photos/airline-tracker-image.jpeg');
+  const flightTrackerGithubURL =
+    'https://github.com/Swislok-Dev/airline_tracker';
+  const flightTrackerLiveURL = 'https://airline-tracker.vercel.app/';
 
   const guitarPicksItems = [
-    "JavaScript frontend",
-    "Ruby login validations",
-    "Rails API",
-    "PostgreSQL",
-  ]
-  const guitarPicksDescription = "Allows users to submit pictures of guitars"
-  const guitarPicksImage = require("../assets/photos/guitar-picks-image.jpeg")
-  const guitarPicksGithubURL = "https://github.com/Swislok-Dev/Guitar-Picks"
-  const guitarPicksLiveURL = "http://guitar-picks.netlify.app"
+    'JavaScript frontend',
+    'Ruby login validations',
+    'Rails API',
+    'PostgreSQL',
+  ];
+  const guitarPicksDescription = 'Allows users to submit pictures of guitars';
+  const guitarPicksImage = require('../assets/photos/guitar-picks-image.jpeg');
+  const guitarPicksGithubURL = 'https://github.com/Swislok-Dev/Guitar-Picks';
+  const guitarPicksLiveURL = 'http://guitar-picks.netlify.app';
 
-  const adphotoItems = ["React"]
+  const adphotoItems = ['React', 'Framer-Motion', 'Lazy loaded images'];
   const adphotoDescription =
-    "Photographer portfolio, showcasing featured images of photographers work"
-  const adphotoImage = require("../assets/photos/adphotography.jpeg")
-  const adphotoGithubURL = "https://github.com/Swislok-Dev/annadahliaphotography"
-  const adphotoLiveURL = "https://annadahlia.com"
+    'Photographer portfolio, showcasing featured images of photographers work';
+  const adphotoImage = require('../assets/photos/adphotography.jpeg');
+  const adphotoGithubURL =
+    'https://github.com/Swislok-Dev/annadahliaphotography';
+  const adphotoLiveURL = 'https://annadahlia.com';
 
   return (
     <section id="projects" className="projects">
@@ -59,5 +62,5 @@ export default function Projects() {
         />
       </div>
     </section>
-  )
+  );
 }
