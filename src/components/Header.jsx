72e59509af83bbb410c177a7971a0857f526a11f
Swislok-Dev <cmarker4@gmail.com>
2022-12-05 11:32:13 -0600

    update prettier & add new skill

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,37 @@
-import React, { useState } from "react"
+import React, { useState } from 'react';
 
 function Header() {
-  const [isActive, setIsActive] = useState(false)
+  const [isActive, setIsActive] = useState(false);
 
   const onHamburgerActive = () => {
-    setIsActive((current) => !current)
-  }
+    setIsActive((current) => !current);
+  };
 
   const showActiveClass = () => {
-    return isActive ? "active" : null
-  }
+    return isActive ? 'active' : null;
+  };
 
   const removeHamburgerActive = () => {
-    setIsActive(false)
-  }
+    setIsActive(false);
+  };
 
   const scrollTo = (scrollId) => {
-    const anchor = document.querySelector(`#${scrollId}`)
+    const anchor = document.querySelector(`#${scrollId}`);
     anchor.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-      inline: "nearest",
-    })
-  }
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
+  };
 
   return (
     <header className="header">
       <nav className="navbar">
-        <a onClick={() => scrollTo("page-hero")} href="#/" className="nav-branding">
+        <a
+          onClick={() => scrollTo('page-hero')}
+          href="#/"
+          className="nav-branding"
+        >
           Cory Marker
         </a>
 
@@ -37,24 +41,36 @@ function Header() {
         >
           <li className="nav-item">
             <a
-              href={require("../assets/photos/Cory Marker Resume.pdf")}
+              href={require('../assets/photos/Cory Marker Resume.pdf')}
               className="nav-link"
             >
               Resume
             </a>
           </li>
           <li className="nav-item">
-            <a onClick={() => scrollTo("projects")} href="#/projects" className="nav-link">
+            <a
+              onClick={() => scrollTo('projects')}
+              href="#/projects"
+              className="nav-link"
+            >
               Projects
             </a>
           </li>
           <li className="nav-item">
-            <a onClick={() => scrollTo("skills")} href="#/skills" className="nav-link">
+            <a
+              onClick={() => scrollTo('skills')}
+              href="#/skills"
+              className="nav-link"
+            >
               Skills
             </a>
           </li>
           <li className="nav-item">
-            <a onClick={() => scrollTo("contact")} href="#/contact" className="nav-link">
+            <a
+              onClick={() => scrollTo('contact')}
+              href="#/contact"
+              className="nav-link"
+            >
               Contact
             </a>
           </li>
@@ -70,7 +86,7 @@ function Header() {
         </div>
       </nav>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import SkillItem from "./skill/SkillItem"
+import React from 'react';
+import SkillItem from './skill/SkillItem';
 
 function Skills() {
   return (
     <section id="skills" className="skills">
       <h2>What I can do</h2>
-      <ul className='skills-list'>
-      <SkillItem skillName="JavaScript"/>
-      <SkillItem skillName="React"/>
-      <SkillItem skillName="Ruby"/>
-      <SkillItem skillName="Ruby on Rails"/>
-      <SkillItem skillName="MongoDB"/>
-      <SkillItem skillName="PostgreSQL"/>
-
+      <ul className="skills-list">
+        <SkillItem skillName="JavaScript" />
+        <SkillItem skillName="React" />
+        <SkillItem skillName="Next.js" />
+        <SkillItem skillName="Ruby" />
+        <SkillItem skillName="Ruby on Rails" />
+        <SkillItem skillName="MongoDB" />
+        <SkillItem skillName="PostgreSQL" />
       </ul>
     </section>
-  )
+  );
 }
 
-export default Skills
+export default Skills;
